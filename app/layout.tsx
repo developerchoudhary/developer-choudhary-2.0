f267cc8cfff38f6c91721f6a8337e6ccbf7fa0b0
Aman Choudhary <amanjaat7668@gmail.com>
2025-07-02 17:02:00 +0530

    update graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og-image.jpg",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: siteConfig.name,
